5b4ebe441e51bd6cfb412200d2a1d5ecd4f8b47d
Murtaza Daudali <murtaza.daudali@student.manchester.ac.uk>
2022-03-29 18:57:56 +0100

    forum now adds to database

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,26 +1,19 @@
 <?php
-$database_host = "dbhost.cs.man.ac.uk";
-$database_user = "m59511md";
-$database_pass = "LeqNGDvbT6VEyF";
-$database_name = "m59511md";
 
-try
-{
-    $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to $database_host successfully.";
+function Connect() {
+    $database_host = "dbhost.cs.man.ac.uk";
+    $database_user = "m59511md";
+    $database_pass = "LeqNGDvbT6VEyF";
+    $database_name = "m59511md";
 
-//    $conn->exec("
-//    INSERT INTO forum (userID, title, description)
-//    VALUES (1, 'wsadas', 'sadsadasa')
-//    ");
-//    echo "ADDED A VALUE";
+    try {
+        $conn = new PDO("mysql:host=$database_host;dbname=$database_name", $database_user, $database_pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "<script>console.log('working!')</script>";
 
+        return $conn;
+
+    } catch (PDOException $pe) {
+        die("Could not connect to $database_host :" . $pe->getMessage());
+    }
 }
-catch (PDOException $pe)
-{
-    die("Could not connect to $database_host :" . $pe->getMessage());
-}
-
-
-$conn = null;
--- scripts/forum.php
+++ scripts/forum.php
@@ -1,122 +1,136 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>AlgoViz</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <title>AlgoViz</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" type="text/css" href="../index.css">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/forum.css">
-  </head>
-  <body>
-    <div class="bg-image" style="background-image: url('images/two.jpg'); height: 150vh;">
+    <body style="background:linear-gradient(to top, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(../assets/two.jpg);">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#" style="color:orangered;">ALGOVIZ</a>
-              <a href="#">
-                <img class="user-icon" src="images/icon.png" alt="user icon">
-              </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+            <a class="navbar-brand " href="../home.html" style="font-weight: bold;font-size:25px; color: #3271a8;">ALGOVIZ</a>
+            <a href="../account1.html">
+            <img class="user-icon" src="../assets/icon.png" alt="user icon">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="collapsibleNavbar">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="nav navbar-nav navbar-center">
                         <!-- <li class="nav-item active">
-                        <a class="nav-link" href="#" style="font-size: 20px; font-family:Helvetica;">Home <span class="sr-only">(current)</span></a>
-                        </li> -->
+                                    <a class="nav-link" href="#" style="font-size: 20px; font-family:Helvetica;">Home <span class="sr-only">(current)</span></a>
+                                    </li> -->
                         <!-- DROP DOWN START ........../ -->
                         <li class="nav-item active dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size: 20px; font-family:Helvetica;">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false" style="font-size: 20px; font-family:Helvetica;">
                             Trees
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                        </div>
-                        </li>
-                        <li class="nav-item active dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size: 20px; font-family:Helvetica;">
-                                Sorts
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="../rbt.html">red black tree</a>
+                            <a class="dropdown-item" href="../bst.html">binary search tree</a>
                             </div>
                         </li>
                         <li class="nav-item active dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size: 20px; font-family:Helvetica;">
-                                Searching
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false" style="font-size: 20px; font-family:Helvetica;">
+                            Sorts
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="../mergesort.html">Merge sort</a>
+                            <a class="dropdown-item" href="../quicksortv2.html">Quick sort</a>
+                            </div>
+                        </li>
+                        <li class="nav-item active dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false" style="font-size: 20px; font-family:Helvetica;">
+                            Searching
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="../bst.html">breadth first search</a>
+                            <a class="dropdown-item" href="../dfs.html">depth first search</a>
                             </div>
                         </li>
                         <!-- LAST DROP DOWN /////////////////// -->
                         <li class="nav-item active dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size: 20px; font-family:Helvetica;">
-                                Others
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false" style="font-size: 20px; font-family:Helvetica;">
+                            Others
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <!-- <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Something else here</a> -->
+                            <a class="dropdown-item" href="../prim.html">Prim</a>
+                            <a class="dropdown-item" href="../KMP.html">KMP</a>
+                            <!-- <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Something else here</a> -->
                             </div>
                         </li>
                     </ul>
                 </div>
                 <!-- Searching barrrrr............. -->
                 <form class="form-inline">
-                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
-        </nav>        
+        </nav>
 
         <div class="content">
-          <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <form method="post" action="homeforum.php?">
-                    <div class="form">
-                    <?php
-                        $topic = $_POST["name_topic"];
-                        $description = $_POST["post_text"];
-                        echo '<table style="width:30em"  border="2em">
-                                <tr>
-                                <th>Topic</th>
-                                <th>Description</th>
-                                </tr>';
+                <div class="container">
+                    <form method="post" action="homeforum.php?">
+                        <?php
+                            include "connect.php";
 
-                        echo '<tr>';
-                            echo '<td class="leftpart">';
-                                echo '<h3>';
-                                echo '<a href="topic.php?data=<?=$topic?>">';
-                                echo $topic;
-                                echo '</a>';
-                                echo '</h3>';
-                            echo '</td>';
-                            echo '<td>';
-                            echo substr($description, 0, 30);
-                            echo '</td>';
-                        echo '</tr>';
-                        echo '<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Add Topic</button><br>';
-                    ?><br>
-                </form>
+                            $topic = $_POST["name_topic"];
+                            $description = $_POST["post_text"];
+                            $conn = Connect();
+                            $sql = sprintf("INSERT INTO forum (userID, title, description) VALUES (1, '%s', '%s')", $topic, $description);
+                            echo "<script>console.log('$sql')</script>";
+                            $conn->exec($sql);
+                            $conn = null;
+
+                            echo '<table style="width:30em; background:white; position:absolute; left:12em; top:13em;"  border="2em">
+                                    <tr>
+                                    <th style="color:dark-grey">Topic</th>
+                                    <th style="color:dark-grey">Description</th>
+                                    </tr>';
+
+                                echo '<tr>';
+                                    echo '<td class="leftpart">';
+                                        echo '<h3>';
+                                        echo '<a href="../topic.php?data=<?=$topic?>">';
+                                        echo $topic;
+                                        echo '</a>';
+                                        echo '</h3>';
+                                    echo '</td>';
+                                    echo '<td style="color:black">';
+                                    echo substr($description, 0, 30);
+                                    echo '</td>';
+                                echo '</tr>';
+                        ?><br>
+                    </form>
+                </div>
+                <button id="buttonTopics" style="position:absolute; left:45em; top:20em;" class="btn btn-outline-primary my-2 my-sm-0" type="submit">Add Topic</button><br>
             </div>
-          </div>
-        </div>
-    </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+    </body>
 
+</html>
 
 <!-- 
   {
