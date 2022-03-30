7ebab338e1cbb061d9a7a149c405d2d335dc9b54
Murtaza Daudali <murtaza.daudali@student.manchester.ac.uk>
2022-03-30 12:24:12 +0100

    databse more funky

--- home.php
+++ home.php
@@ -12,9 +12,58 @@
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
-<body >
+<body>
+
+<?php
+    include 'scripts/connect.php';
+
+    function SignUp() {
+        $conn = Connect();
+
+        $username = $_POST['uname'];
+        $email = $_POST['email'];
+        $password = $_POST['psw'];
+        $re_password = $_POST['re-psw'];
+
+        $sql = sprintf("
+    INSERT INTO users (name, password, email)
+    VALUES ('%s', '%s', '%s')
+    ", $username, $password, $email);
+
+        $conn->exec($sql);
+        $conn = null;
+    }
+
+    function LogIn() {
+        $conn = Connect();
+
+        $username = $_POST['uname2'];
+        $password = $_POST['psw2'];
+
+        $stmt = $conn->query("SELECT name, password FROM users");
+        $stmt->setFetchMode(PDO::FETCH_NUM);
+
+        foreach ($stmt as $row) {
+            $curr_username = $row[0];
+            $curr_password = $row[1];
+
+            if ($curr_username == $username and $curr_password == $password) {
+                echo "<script>console.log('Logged in!!')</script>";
+            }
+        }
+    }
+
+    if (!empty($_POST['uname2'])) {
+        LogIn();
+    } else if (!empty($_POST['uname'])) {
+        SignUp();
+    } else {
+        echo "<script>console.log('goofy')</script>";
+    }
+
+?>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand " href="home.html" style="font-weight: bold;font-size:25px; color: #3271a8;">ALGOVIZ</a>
+    <a class="navbar-brand " href="home.php" style="font-weight: bold;font-size:25px; color: #3271a8;">ALGOVIZ</a>
     <a href="account1.html">
       <img class="user-icon" src="assets/icon.png" alt="user icon">
     </a>
